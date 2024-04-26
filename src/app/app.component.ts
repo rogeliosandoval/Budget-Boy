4427f04432b8cf2bcf456c183c18e750b234a97a
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-04-25 19:15:01 -0500

    update the doom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  title = 'budget-boy';
+export class AppComponent implements OnInit {
+  public lightMode: boolean = true;
+  public authService = inject(AuthService);
+
+  ngOnInit(){
+    this.authService.user$.subscribe((user) => {
+      if (user) {
+        this.authService.currentUserSignal.set({
+          email: user.email!,
+          username: user.displayName!
+        });
+      } else {
+        this.authService.currentUserSignal.set(null);
+      }
+    });
+  }
+
+  public logout(): void {
+    this.authService.logout();
+  }
 }
