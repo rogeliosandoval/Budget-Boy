b4f6eeac9552b90fe8d5ac9845c14b3cbff04082
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-04-29 21:52:05 -0500

    add guards

---
+++ src/app/auth.user.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from "@angular/core";
+import { AuthService } from "./services/auth.service";
+import { Router } from "@angular/router";
+import { map } from "rxjs";
+
+export const AuthUserGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.user$.pipe(
+    map(data => {
+      if (data === null) {
+        router.navigateByUrl('/login');
+        return false;
+      }
+      return true;
+    })
+  )
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, inject, signal } from "@angular/core";
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, user } from "@angular/fire/auth";
+import { Observable, from } from "rxjs";
+import { User } from "../user.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  firebaseAuth = inject(Auth);
+  user$ = user(this.firebaseAuth);
+  currentUserSignal = signal<User | undefined | null>(undefined)
+
+  register(
+    username: string,
+    email: string,
+    password: string
+  ): Observable<void> {
+    const promise = createUserWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((response) => 
+      updateProfile(response.user, { displayName: username })
+    );
+
+    return from(promise);
+  }
+
+  login(email: string, password: string): Observable<void> {
+    const promise = signInWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then(() => {});
+    return from(promise);
+  }
+
+  logout(): Observable<void> {
+    const promise = signOut(this.firebaseAuth);
+    return from(promise);
+  }
+}
