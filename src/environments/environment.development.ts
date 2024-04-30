b4523875af0e3e1ba21352bd40d8f1a713efd4f3
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-04-29 21:51:20 -0500

    implement firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDa_90sTHXXzwpmClIlTDKLLWJkmneqZuk",
+    authDomain: "budget-boy-ac3a5.firebaseapp.com",
+    projectId: "budget-boy-ac3a5",
+    storageBucket: "budget-boy-ac3a5.appspot.com",
+    messagingSenderId: "1023239933988",
+    appId: "1:1023239933988:web:aa4453155de3b5436148b1"
+  },
+  production: true
+}
