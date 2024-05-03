29f6657febedbdcad39a5ce6fa270ee1fc44ab90
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-05-02 22:40:15 -0500

    update login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,12 +11,13 @@ import { Router } from "@angular/router";
 
 export class LoginComponent {
   public loginForm: FormGroup;
+  public errorMessage: string = '';
+  public loading: boolean = false;
   private authService = inject(AuthService);
   private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
 
-  constructor(
-    private formBuilder: FormBuilder
-  ){
+  constructor(){
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
@@ -24,10 +25,22 @@ export class LoginComponent {
   }
 
   public login(): void {
+    this.loading = true;
     const rawForm = this.loginForm.getRawValue();
+
     this.authService.login(rawForm.email, rawForm.password)
-      .subscribe(() => {
-        this.router.navigateByUrl('/dashboard');
-      });
+    .subscribe({
+      next: response => {
+        this.router.navigateByUrl('/home');
+      },
+      error: err => {
+        this.loading = false;
+        if (err.code === 'auth/invalid-credential') {
+          this.errorMessage = 'Wrong email or password. Please try again.'
+        } else {
+          this.errorMessage = err.code
+        }
+      }
+    })
   }
 }
