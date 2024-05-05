cd4447355dafab237e059d0993209a91b21abd0f
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-05-05 12:03:27 -0500

    update the dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
+import { SharedService } from "../services/shared.service";
+import { ExpenseFormComponent } from "../dialogs/expense-form/expense-form.component";
+import { MatDialog } from "@angular/material/dialog";
+import { ExpenseService } from "src/app/services/expense.service";
+import { Expense } from "../user.interface";
 
 @Component({
   selector: 'dashboard',
@@ -6,6 +11,43 @@ import { Component } from "@angular/core";
   styleUrls: ['dashboard.component.scss']
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  public sharedService = inject(SharedService);
+  private matDialog = inject(MatDialog);
+  private expenseService = inject(ExpenseService);
+  public expenses: Expense[] = [];
+  public totalExpenses = 0;
 
+  ngOnInit(){
+    this.getAllExpenses();
+  }
+
+  public addExpense(): void {
+    const dialogRef = this.matDialog.open(ExpenseFormComponent, {
+      data: {
+
+      }
+    });
+  }
+
+  public getAllExpenses(): void {
+    this.expenseService.getAllExpenses().snapshotChanges().subscribe({
+      next: (data) => {
+        this.expenses = [];
+
+        data.forEach((item) => {
+          let expense = item.payload.toJSON() as Expense;
+          this.totalExpenses += parseInt(expense.cost);
+          this.expenses.push({
+            key: item.key || '',
+            user: this.sharedService.user.displayName,
+            cost: expense.cost,
+            title: expense.title,
+            note: expense.note
+          })
+        })
+        console.log(this.expenses);
+      }
+    })
+  }
 }
