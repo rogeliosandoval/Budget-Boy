b7bc440a1128978e1927ff5cbd64ee6860bb52e2
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-05-06 17:31:24 -0500

    update config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,7 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyDa_90sTHXXzwpmClIlTDKLLWJkmneqZuk",
     authDomain: "budget-boy-ac3a5.firebaseapp.com",
+    databaseURL: "https://budget-boy-ac3a5-default-rtdb.firebaseio.com",
     projectId: "budget-boy-ac3a5",
     storageBucket: "budget-boy-ac3a5.appspot.com",
     messagingSenderId: "1023239933988",
