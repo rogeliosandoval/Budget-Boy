ef8838d4244a899519db91733cc358550a66f6ad
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-05-07 23:13:18 -0500

    update interfaces

--- src/app/user.interface.ts
+++ src/app/user.interface.ts
@@ -2,3 +2,11 @@ export interface User {
   username: string;
   email: string;
 }
+
+export interface Expense {
+  key?: string;
+  type: string;
+  amount: string;
+  title: string;
+  note: string;
+}
