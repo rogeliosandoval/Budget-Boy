4dfb8dcfcc6927420432da254b79905519ca9e27
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-05-19 22:21:54 -0500

    update decimal pipe

--- src/app/two-decimal.pipe.ts
+++ src/app/two-decimal.pipe.ts
@@ -5,6 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TwoDecimalPipe implements PipeTransform {
   transform(value: number): string {
+    if (isNaN(value)) {
+      return '0.00'; // Handle NaN values gracefully
+    }
     return value.toFixed(2);
   }
 }
