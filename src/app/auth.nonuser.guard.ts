e8d9e89aad864dbb99c1a68ed5cb2a0f21420b5b
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-05-29 21:52:16 -0500

    update guards

--- src/app/auth.nonuser.guard.ts
+++ src/app/auth.nonuser.guard.ts
@@ -1,19 +1,19 @@
-import { inject } from "@angular/core";
-import { AuthService } from "./services/auth.service";
-import { Router } from "@angular/router";
-import { map } from "rxjs";
+import { inject } from "@angular/core"
+import { AuthService } from "./services/auth.service"
+import { Router } from "@angular/router"
+import { map } from "rxjs"
 
 export const AuthNonuserGuard = () => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
+  const authService = inject(AuthService)
+  const router = inject(Router)
 
   return authService.user$.pipe(
     map(data => {
       if (data !== null) {
         router.navigateByUrl('/')
-        return false;
+        return false
       }
-      return true;
+      return true
     })
   )
 }
