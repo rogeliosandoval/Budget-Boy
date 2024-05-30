dbf502772d8eadf5b151d3576efe0784d0488ebc
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-05-29 21:51:50 -0500

    update register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { AuthService } from "../services/auth.service";
-import { SharedService } from "../services/shared.service";
-import { Router } from "@angular/router";
-import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { AngularFireList } from '@angular/fire/compat/database';
+import { Component, inject } from "@angular/core"
+import { FormGroup, FormBuilder, Validators } from "@angular/forms"
+import { AuthService } from "../services/auth.service"
+import { SharedService } from "../services/shared.service"
+import { Router } from "@angular/router"
+import { AngularFireDatabase } from '@angular/fire/compat/database'
+import { AngularFireList } from '@angular/fire/compat/database'
 
 @Component({
   selector: 'register',
@@ -13,16 +13,16 @@ import { AngularFireList } from '@angular/fire/compat/database';
 })
 
 export class RegisterComponent {
-  private database = inject(AngularFireDatabase);
+  private database = inject(AngularFireDatabase)
   private databasePath = '/users/'
-  private authService = inject(AuthService);
-  private sharedService = inject(SharedService);
-  private formBuilder = inject(FormBuilder);
-  private router = inject(Router);
+  private authService = inject(AuthService)
+  private sharedService = inject(SharedService)
+  private formBuilder = inject(FormBuilder)
+  private router = inject(Router)
   public usersRef: AngularFireList<any>
-  public registerForm: FormGroup;
-  public loading: boolean = false;
-  public errorMessage: string = '';
+  public registerForm: FormGroup
+  public loading: boolean = false
+  public errorMessage: string = ''
 
   constructor(){
     this.registerForm = this.formBuilder.group({
@@ -30,15 +30,15 @@ export class RegisterComponent {
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required]
-    });
+    })
 
-    this.usersRef = this.database.list(this.databasePath);
+    this.usersRef = this.database.list(this.databasePath)
   }
 
   public register(): void {
-    this.loading = true;
-    this.sharedService.navFade = true;
-    const rawForm = this.registerForm.getRawValue();
+    this.loading = true
+    this.sharedService.navFade = true
+    const rawForm = this.registerForm.getRawValue()
     const databaseForm = {
       username: this.registerForm.get('username')?.value,
       email: this.registerForm.get('email')?.value,
@@ -48,12 +48,12 @@ export class RegisterComponent {
     this.authService.register(rawForm.username, rawForm.email, rawForm.password)
       .subscribe({
         next: response => {
-          this.usersRef.push(databaseForm);
-          window.location.reload();
+          this.usersRef.push(databaseForm)
+          window.location.reload()
         },
         error: err => {
-          this.loading = false;
-          this.sharedService.navFade = false;
+          this.loading = false
+          this.sharedService.navFade = false
           if (err.code === 'auth/email-already-in-use') {
             this.errorMessage = 'Email is laready in use. Please use another email.'
           } else {
