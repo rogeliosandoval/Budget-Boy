df8ebb998cfee4f6d83df20c9b3ec69cd362db41
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-06-04 20:19:27 -0500

    update forms

--- src/app/dialogs/funds-form/funds-form.component.ts
+++ src/app/dialogs/funds-form/funds-form.component.ts
@@ -26,8 +26,38 @@ export class FundsFormComponent implements OnInit {
 
   ngOnInit(){}
 
+  public handleControlNumberValue(): void {
+    const amountControl = this.fundsForm.get('amount')
+    if (amountControl) {
+      let amountValue = amountControl.value
+  
+      // Remove commas before checking for a decimal point
+      const amountWithoutCommas = amountValue.replace(/,/g, '')
+  
+      // Check if value does not contain a decimal point
+      if (amountWithoutCommas && amountWithoutCommas.indexOf('.') === -1) {
+        // Format the value to add commas for thousands
+        amountValue = amountWithoutCommas.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        
+        // Append .00 if there is no decimal point
+        amountValue += '.00'
+  
+        // Update the form control value
+        amountControl.setValue(amountValue)
+      } else {
+        // If a decimal point is present, format the value with commas for thousands
+        amountValue = amountWithoutCommas.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        // Update the form control value
+        amountControl.setValue(amountValue)
+      }
+    }
+  }
+
   public onSubmit(): void {
-    this.expenseService.addAvailableFunds(this.fundsForm.get('amount')?.value)
-    this.dialogRef.close(this.fundsForm.get('amount')?.value)
+    this.handleControlNumberValue()
+    const amountControl = this.fundsForm.get('amount')
+    const amountValue = parseFloat(amountControl?.value.replace(/,/g, ''))
+    this.expenseService.addAvailableFunds(amountValue)
+    this.dialogRef.close(amountValue)
   }
 }
